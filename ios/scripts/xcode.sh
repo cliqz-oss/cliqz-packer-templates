71aa100d3fbbf96e12bd2102c14de676749f8aab
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-08-30 17:29:24 +0200

    X

--- ios/scripts/xcode.sh
+++ ios/scripts/xcode.sh
@@ -4,10 +4,10 @@ set -eo pipefail
 shopt -s nullglob
 
 curl -O https://gist.githubusercontent.com/pudquick/ff412bcb29c9c1fa4b8d/raw/24b25538ea8df8d0634a2a6189aa581ccc6a5b4b/parse_pbzx2.py
-python parse_pbzx2.py Content
 
 cd /private/tmp
 xar -xf Xcode.xip
+python parse_pbzx2.py Content
 xz -d Content.part00.cpio.xz
 sudo cpio -idm < ./Content.part00.cpio
 mv Xcode.app /Applications/Xcode.app
