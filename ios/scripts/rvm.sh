a95b88429bddc9971220e2cee07f8cc9046bdcd8
Leonid Rashkovsky <leonid@cliqz.com>
2017-09-19 14:56:01 +0200

    Added some echos

--- ios/scripts/rvm.sh
+++ ios/scripts/rvm.sh
@@ -2,6 +2,7 @@
 
 sudo -u vagrant -H /bin/bash --login -c 'echo "gem: --no-document" >> ~/.gemrc'
 sudo -u vagrant -H /bin/bash --login -c 'curl -L https://get.rvm.io | bash -s stable --auto-dotfiles --autolibs=enable'
+echo "Installing ruby version ${RUBY_VERSION}"
 sudo -u vagrant -H /bin/bash --login -c ' \
 	rvm install ${RUBY_VERSION} && \
 	rvm use ${RUBY_VERSION} 	&& \
