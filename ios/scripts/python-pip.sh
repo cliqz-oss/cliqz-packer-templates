e26b7b5fb9ef68995541290ee1fd98c53086cf4d
joseph-cliqz <joseph@cliqz.com>
2018-05-15 17:11:44 +0200

    Build HighSierra Image with Xcode 9.3

--- ios/scripts/python-pip.sh
+++ ios/scripts/python-pip.sh
@@ -6,3 +6,4 @@ sudo -u vagrant -H /bin/bash --login -c 'python get-pip.py'
 sudo -u vagrant -H /bin/bash --login -c 'pip install compare-locales'
 
 sudo -u vagrant -H /bin/bash --login -c 'npm -g install appdmg'
+sudo -u vagrant -H /bin/bash --login -c 'brew install python2'
